40e4fabed31be63007462d4d56cfd1fef7cf183c
JovannMC <jovannmc@femboyfurry.net>
2024-07-04 01:26:29 +0300

    Add socials and "cool links"

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,9 +1,27 @@
+<script>
+	const socialLinks = [
+		{ name: 'Discord', url: '/images/socials/discord.svg', link: 'https://discord.com/invite/fPrjXeN9fe' },
+		{ name: 'Matrix', url: '/images/socials/matrix.svg', link: 'https://matrix.to/#/@jovannmc:tchncs.de' },
+		{ name: 'GitHub', url: '/images/socials/github.svg', link: 'https://github.com/JovannMC' },
+		{ name: 'Twitter', url: '/images/socials/twitter.svg', link: 'https://twitter.com/JovannMC' },
+		{ name: 'Mastodon', url: '/images/socials/mastodon.svg', link: 'https://mastodon.femboyfurry.net/@JovannMC' },
+		{ name: 'YouTube', url: '/images/socials/youtube.svg', link: 'https://youtube.com/@JovannMC' },
+		{ name: 'Vanillo', url: '/images/socials/vanillo.svg', link: 'https://vanillo.tv/u/JovannMC' },
+		{ name: 'Twitch', url: '/images/socials/twitch.svg', link: 'https://twitch.tv/JovannMC' },
+		{ name: 'PeerTube', url: '/images/socials/peertube.svg', link: 'https://vid.femboyfurry.net/c/jovannmc' },
+		{ name: 'Odysee', url: '/images/socials/odysee.svg', link: 'https://odysee.com/@JovannMC:7' }
+	];
+
+	let firstRowLinks = socialLinks.slice(0, Math.ceil(socialLinks.length / 2));
+	let secondRowLinks = socialLinks.slice(Math.ceil(socialLinks.length / 2));
+</script>
+
 <footer class="bg-surface-800 text-white py-4">
 	<div class="container mx-auto px-4">
 		<div class="flex flex-col md:flex-row justify-around items-center space-y-4 md:space-y-0">
 			<!-- Logo and message -->
 			<div class="flex-1 px-2 text-center md:text-left md:ml-4">
-				<img src="/images/jovannmc_white.png" alt="Logo" class="h-8 mb-4 mx-auto md:mx-0" />
+				<img src="/images/jovannmc_white.png" alt="Logo" class="h-10 mb-4 mx-auto md:mx-0" />
 				<p class="w-full md:w-9/12 mx-auto md:mx-0">
 					Hey, thanks for scrolling to the end of the page. The site is <a
 						href="https://github.com/JovannMC/jovann.me"
@@ -18,34 +36,32 @@
 				<div class="w-full text-center">
 					<h3 class="font-bold mb-2">Cool links</h3>
 					<ul class="flex flex-row md:flex-col md:gap-1 gap-6 justify-center">
-						<li><a href="/" class="hover:underline">Link here</a></li>
-						<li><a href="/" class="hover:underline">Link here</a></li>
-						<li><a href="/" class="hover:underline">Link here</a></li>
+						<li><a href="https://litdevs.org/" class="hover:underline" target="_blank">LIT Devs</a></li>
+						<li><a href="https://github.com/OCSYT/SlimeTora" class="hover:underline" target="_blank">SlimeTora</a></li>
+						<li><a href="https://github.com/SlimeVR/" class="hover:underline" target="_blank">SlimeVR</a></li>
 					</ul>
 				</div>
 			</div>
 
 			<!-- Social media icons -->
-			<div class="flex-1 text-right px-2">
+			<div class="flex-1 text-center md:text-right px-2">
 				<div>
-					<h3 class="font-bold mb-2">Find me</h3>
-					<div class="inline-flex justify-end space-x-2">
-						<a href="https://twitter.com/JovannMC" target="_blank" class="hover:text-blue-400">
-							<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"
-								><path
-									d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-2.719 0-4.92 2.201-4.92 4.917 0 .386.044.763.128 1.124-4.087-.205-7.715-2.164-10.144-5.144-.424.729-.666 1.574-.666 2.476 0 1.71.869 3.213 2.188 4.096-.806-.026-1.566-.247-2.228-.616v.062c0 2.386 1.698 4.374 3.946 4.827-.413.112-.848.171-1.296.171-.316 0-.624-.03-.924-.086.625 1.953 2.441 3.377 4.6 3.415-1.68 1.318-3.808 2.103-6.115 2.103-.398 0-.79-.023-1.175-.069 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.143-7.577 14.143-14.143 0-.216-.005-.432-.014-.648.975-.703 1.821-1.579 2.491-2.578z"
-								/></svg
-							>
-						</a>
-						<a href="https://github.com/JovannMC" target="_blank" class="hover:text-gray-400">
-							<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"
-								><path
-									fill-rule="evenodd"
-									d="M12 2C6.477 2 2 6.477 2 12c0 4.418 2.865 8.166 6.839 9.489.5.092.682-.217.682-.482 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.155-1.11-1.462-1.11-1.462-.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.111-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.269 2.75 1.025A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.294 2.747-1.025 2.747-1.025.546 1.377.203 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.841-2.337 4.687-4.564 4.933.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.743 0 .267.18.578.688.481A10.001 10.001 0 0022 12c0-5.523-4.477-10-10-10z"
-									clip-rule="evenodd"
-								/></svg
-							>
-						</a>
+					<h3 class="font-bold mb-4 md:mb-2">Find me</h3>
+					<!-- First row of icons -->
+					<div class="flex justify-end md:gap-4 gap-8 md:mb-2 mb-4">
+						{#each firstRowLinks as { name, url, link }}
+							<a href={link} target="_blank" class="w-8 h-8 md:w-7 md:h-7">
+								<img class="w-8 h-8 md:w-7 md:h-7" src={url} alt={name} />
+							</a>
+						{/each}
+					</div>
+					<!-- Second row of icons -->
+					<div class="flex justify-end md:gap-4 gap-8">
+						{#each secondRowLinks as { name, url, link }}
+							<a href={link} target="_blank" class="w-8 h-8 md:w-7 md:h-7">
+								<img class="w-8 h-8 md:w-7 md:h-7" src={url} alt={name} />
+							</a>
+						{/each}
 					</div>
 				</div>
 			</div>
